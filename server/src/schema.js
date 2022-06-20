f2bcdccb7e7d6951a74390fadac950993d818449
櫻田亮太 <tns.r.14xx@gmail.com>
2022-06-20 23:18:32 +0900

    query

--- server/src/schema.js
+++ server/src/schema.js
@@ -3,6 +3,7 @@ const { gql } = require("apollo-server");
 const typeDefs = gql`
   type Query {
     tracksForHome: [Track!]!
+    track(id: ID!): Track
   }
   type Track {
     id: ID!
@@ -11,11 +12,19 @@ const typeDefs = gql`
     thumbnail: String
     length: Int
     modulesCount: Int
+    description: String
+    numberOfViews: Int
+    modules: [Module!]!
   }
   type Author {
     id: ID!
     name: String!
     photo: String
+  }
+  type Module {
+    id: ID!
+    title: String!
+    length: Int
   }
 `;
 module.exports = typeDefs;
